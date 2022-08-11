200c994a71e67357e725a92f8a6002858d899b04
Lucas Cantoni <lukcnt@outlook.com>
2022-08-11 15:59:12 -0300

    Added the message that shows the result of the sum.

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ sum_even_numbers = 0
 for number in range(1, 101):
     if number % 2 == 0:
         sum_even_numbers += number
+
+print(f"The sum of all even numbers from 1 to 100 is: {sum_even_numbers}.")
